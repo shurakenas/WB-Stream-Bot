a058f2c4f00a379fc8359bd31115ff119fd566ab
shurakenas <45785038+shurakenas@users.noreply.github.com>
2026-06-11 21:14:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ ALLOWED_USER_IDS = []  # ТВОЙ ID (можно добавить других �
 FIXED_ROOM_NAME = ""
 
 # Путь к папке с куками
-COOKIES_DIR = "/opt/bot/cookies"
+COOKIES_DIR = "/opt/WB-Stream-Bot/cookies"
 
 # Список файлов кук (для ротации аккаунтов)
 COOKIES_FILES = [
@@ -16,7 +16,7 @@ COOKIES_FILES = [
     "acc2.json",
 ]
 
-CREATOR_BIN = "/opt/bot/headless/headless-wbstream-creator"
+CREATOR_BIN = "/opt/WB-Stream-Bot/headless/headless-wbstream-creator"
 
 # Интервалы ротации в минутах (случайный выбор)
 ROTATION_INTERVALS = [25, 45, 65]
